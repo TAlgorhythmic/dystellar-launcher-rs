82c784d859f4980d403f67290fa36f681474b593
Algorhythmics <intivioli@gmail.com>
2025-05-06 15:33:16 +0200

    Implement requests with hyper

--- src/api/control/http.rs
+++ src/api/control/http.rs
@@ -1,10 +1,40 @@
+use std::error::Error;
+
+use hyper::{body::Incoming, client::conn::http1::handshake, Request, Response, Uri};
+use hyper_util::rt::TokioIo;
+use json::JsonValue;
+use tokio::net::TcpStream;
 use webbrowser;
 use uuid::Uuid;
 
 const CLIENT_ID: &str = env!("CLIENT_ID");
 const BACKEND_URL: &str = env!("BACKEND_URL");
 
+pub async fn request(url: &str, method: &str, body: JsonValue) -> Result<Response<Incoming>, Box<dyn Error + Send + Sync>> {
+    let url: Uri = url.parse()?;
 
+    let host = url.host().unwrap();
+    let port = url.port_u16().unwrap_or(80);
+
+    let address = format!("{}:{}", host, port);
+    
+    let stream = TcpStream::connect(address).await?;
+    let io = TokioIo::new(stream);
+
+    let (mut sender, _) = handshake(io).await?;
+
+    let authority = url.authority().unwrap();
+
+    let req = Request::builder()
+        .uri(&url)
+        .header(hyper::header::HOST, authority.as_str())
+        .method(method)
+        .body(String::from(stringify!(body)))?;
+
+    let res = sender.send_request(req).await?;
+
+    Ok(res)
+}
 
 pub fn login() {
     let uuid = Uuid::new_v4();
@@ -17,7 +47,7 @@ pub fn login() {
 
     tokio::spawn(async move {
         if webbrowser::open(&ms_url).is_ok() {
-
+            
         }
     });
 }
