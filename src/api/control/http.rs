a0cab4495389c565bae090ba2ee649ffbc1f98e3
Algorhythmics <intivioli@gmail.com>
2025-05-08 08:43:00 +0200

    Update

--- src/api/control/http.rs
+++ src/api/control/http.rs
@@ -1,8 +1,9 @@
-use std::error::Error;
+use std::{error::Error, fmt::Write, io::Read};
 
-use hyper::{body::Incoming, client::conn::http1::handshake, Request, Response, Uri};
+use http_body_util::BodyExt;
+use hyper::{body::{self, Incoming}, client::conn::http1::handshake, Request, Response, Uri};
 use hyper_util::rt::TokioIo;
-use json::JsonValue;
+use json::{object, JsonValue};
 use tokio::net::TcpStream;
 use webbrowser;
 use uuid::Uuid;
@@ -10,8 +11,9 @@ use uuid::Uuid;
 const CLIENT_ID: &str = env!("CLIENT_ID");
 const BACKEND_URL: &str = env!("BACKEND_URL");
 
-pub async fn request(url: &str, method: &str, body: JsonValue) -> Result<Response<Incoming>, Box<dyn Error + Send + Sync>> {
-    let url: Uri = url.parse()?;
+pub async fn request(path: &str, method: &str, body: JsonValue) -> Result<Response<Incoming>, Box<dyn Error + Send + Sync>> {
+    let abspath = format!("{}{}", BACKEND_URL, path);
+    let url: Uri = abspath.parse()?;
 
     let host = url.host().unwrap();
     let port = url.port_u16().unwrap_or(80);
@@ -40,14 +42,30 @@ pub fn login() {
     let uuid = Uuid::new_v4();
     println!("{uuid}");
 
-    let mut callback = String::from(BACKEND_URL);
-    callback.push_str("/api/microsoft/callback");
+    let callback = format!("{BACKEND_URL}/api/microsoft/callback");
 
     let ms_url = format!("https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id={}&response_type=code&redirect_uri={}&scope=XboxLive.signin%20offline_access&state={}", CLIENT_ID, callback, &uuid);
 
     tokio::spawn(async move {
+        let uuid_owned = uuid;
+        let lsopt = request(format!("/api/microsoft/loginsession?uuid={uuid_owned}").as_str(), "POST", object! {}).await;
+        if lsopt.is_err() {
+            // TODO: Error trying to connect to server
+            println!("Error connecting to server.");
+            return;
+        }
+        
+        let lsession_res = lsopt.unwrap();
+        let bod = lsession_res.into_body().collect().await.unwrap().to_bytes();
+        let js = json::parse(String::from_utf8(bod.to_vec()).unwrap().as_str()).unwrap();
+        
+        if !js["ok"].as_bool().unwrap() {
+            // TODO: Server error
+            println!("Server error");
+            return;
+        }
+
         if webbrowser::open(&ms_url).is_ok() {
-            
         }
     });
 }
