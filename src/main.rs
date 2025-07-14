7082c6c9509626930898fdc978364482bd3399a7
Algorhythmics <intivioli@gmail.com>
2025-07-14 20:21:07 +0200

    Debug backend

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,10 @@ mod api;
 mod utils;
 
 fn main() -> glib::ExitCode {
+    if let Ok(gsk) = std::env::var("GSK_RENDERER") {
+        println!("{gsk}");
+    } else {
+        println!("No backend specified.");
+    }
     ui::launcher::run()
 }
