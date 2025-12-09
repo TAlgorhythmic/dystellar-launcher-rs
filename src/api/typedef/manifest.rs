25e909e777e4f652ab61ce74a0db915452b7a973
Algorhythmics <intivioli@gmail.com>
2025-12-09 17:46:00 +0100

    Update

--- src/api/typedef/manifest.rs
+++ src/api/typedef/manifest.rs
@@ -36,24 +36,60 @@ pub struct MinecraftManifest {
 impl TryFrom<JsonValue> for AssetIndex {
     type Error = Box<dyn Error + Send + Sync>;
 
-    fn try_from(value: JsonValue) -> Result<Self, Self::Error> {
-        todo!()
+    fn try_from(v: JsonValue) -> Result<Self, Self::Error> {
+        Ok(Self {
+            id: v["id"].as_str().ok_or("assetIndex.id missing")?.into(),
+            sha1: v["sha1"].as_str().ok_or("assetIndex.sha1 missing")?.into(),
+            size: v["size"].as_isize().ok_or("assetIndex.size missing")?,
+            total_size: v["totalSize"].as_isize().ok_or("assetIndex.totalSize missing")?,
+            url: v["url"].as_str().ok_or("assetIndex.url missing")?.into(),
+        })
     }
 }
 
-impl TryFrom<JsonValue> for Download {
+impl TryFrom<(&str, &JsonValue)> for Download {
     type Error = Box<dyn Error + Send + Sync>;
 
-    fn try_from(value: JsonValue) -> Result<Self, Self::Error> {
-        todo!()
+    fn try_from(v: (&str, &JsonValue)) -> Result<Self, Self::Error> {
+        let (id, v) = v;
+        Ok(Self {
+            id: id.into(),
+            path: v["path"].as_str().map(|s| s.into()),
+            sha1: v["sha1"].as_str().ok_or("download.sha1 missing")?.into(),
+            size: v["size"].as_isize().ok_or("download.size missing")?,
+            url: v["url"].as_str().ok_or("download.url missing")?.into(),
+        })
     }
 }
 
 impl TryFrom<JsonValue> for Library {
     type Error = Box<dyn Error + Send + Sync>;
 
-    fn try_from(value: JsonValue) -> Result<Self, Self::Error> {
-        todo!()
+    fn try_from(v: JsonValue) -> Result<Self, Self::Error> {
+        let mut dl_vec = vec![];
+
+        for item in v["downloads"].entries() {
+            dl_vec.push(Download::try_from(item)?);
+        }
+
+        // OS rules
+        let mut os_vec = vec![];
+
+        if let JsonValue::Array(rules) = &v["rules"] {
+            for rule in rules {
+                if rule["action"] == "allow" {
+                    if let Some(name) = rule["os"]["name"].as_str() {
+                        os_vec.push(name.into());
+                    }
+                }
+            }
+        }
+
+        Ok(Self {
+            downloads: dl_vec,
+            name: v["name"].as_str().ok_or("library.name missing")?.into(),
+            os: os_vec,
+        })
     }
 }
 
@@ -61,6 +97,23 @@ impl TryFrom<JsonValue> for MinecraftManifest {
     type Error = Box<dyn Error + Send + Sync>;
 
     fn try_from(value: JsonValue) -> Result<Self, Self::Error> {
-        todo!()
+        let mut downloads: Vec<Download> = vec![];
+        let mut libs: Vec<Library> = vec![];
+
+        for download in value["downloads"].entries() {
+            downloads.push(Download::try_from(download)?);
+        }
+        for lib in value["libraries"].members() {
+            libs.push(Library::try_from(lib.clone())?);
+        }
+
+        Ok(Self {
+            args: vec![],
+            compliance_level: value["complianceLevel"].as_i32().ok_or("complianceLevel missing")?,
+            downloads,
+            java_version: value["javaVersion"]["majorVersion"].as_i32().ok_or("javaVersion.majorVersion missing")?,
+            libs,
+            main_class: value["mainClass"].as_str().ok_or("mainClass missing")?.into()
+        })
     }
 }
