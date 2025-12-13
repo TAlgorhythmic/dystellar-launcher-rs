90b91a40e71925c75ebcdeb649f28f36a2c124aa
Algorhythmics <intivioli@gmail.com>
2025-12-13 23:46:36 +0100

    Task manager finished first version

--- src/api/typedef/task_manager.rs
+++ src/api/typedef/task_manager.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, rc::Rc, sync::{Arc, Mutex, atomic::{AtomicBool, AtomicI32, Ordering}, mpsc::{self, Receiver, Sender}}, thread, time::Duration};
+use std::{collections::HashMap, rc::Rc, sync::{Arc, Condvar, Mutex, atomic::{AtomicBool, AtomicI16, AtomicI32, Ordering}, mpsc::{self, Receiver, Sender}}, thread, time::Duration};
 
 use slint::{Model, ModelRc, Timer, TimerMode, VecModel};
 
@@ -11,6 +11,7 @@ pub trait Task: Send + Sync + 'static {
     fn get_progress(&self) -> f32;
     fn get_state(&self) -> TaskState;
     fn get_id(&self) -> i32;
+    fn claim(&self);
 }
 
 pub struct TaskManager {
@@ -18,32 +19,56 @@ pub struct TaskManager {
     tasks: Arc<Mutex<HashMap<i32, Arc<dyn Task>>>>,
     timer: Timer,
     running: AtomicBool,
-    queue: Option<(Sender<Arc<dyn Task>>, Receiver<Arc<dyn Task>>)>
+    semaphore: Arc<Semaphore>,
+    threads: i16
 }
 
 impl TaskManager {
     pub fn new(model: Rc<VecModel<TasksGroup>>) -> Self {
+        let threads = thread::available_parallelism().unwrap().get() as i16;
+
         Self {
             groups_ui: model,
             tasks: Arc::new(Mutex::new(HashMap::new())),
             timer: Timer::default(),
             running: AtomicBool::new(false),
-            queue: None
+            semaphore: Arc::new(Semaphore::new(0)),
+            threads
         }
     }
 
-    fn destroy_queue(&mut self) {
-        drop(self.queue.take());
-    }
-
     fn start_threads(&self) {
-        if let Some((s, r)) = self.queue {
-            
-            for _ in 0..thread::available_parallelism().unwrap().get() {
-                thread::spawn(|| {
+        for _ in 0..self.threads {
+            let semaphore = self.semaphore.clone();
+            let map = self.tasks.clone();
+
+            thread::spawn(move || {
+                loop {
+                    let guard = map.lock().unwrap();
+                    if guard.is_empty() {
+                        return;
+                    }
+
+                    let entry = guard.iter().find(|e| e.1.get_state() == TaskState::Waiting);
+                    if entry.is_none() {
+                        semaphore.acquire();
+                        semaphore.release();
+                        continue;
+                    }
                     
-                })
-            }
+                    let (_, task) = entry.unwrap();
+                    let task = task.clone();
+                    task.claim();
+
+                    drop(guard);
+                    semaphore.acquire();
+                    task.run();
+
+                    let mut guard = map.lock().unwrap();
+                    guard.remove(&task.get_id());
+                    semaphore.release();
+                }
+            });
         }
     }
 
@@ -73,6 +98,26 @@ impl TaskManager {
                     group.get_model().set_row_data(j, task);
                 }
             }
+            if !removals.is_empty() {
+                let mut i = 0;
+                while i < groups_ui.row_count() {
+                    let group = groups_ui.row_data(i).unwrap();
+                    let mut j = 0;
+                    while j < group.tasks.row_count() {
+                        let task = group.tasks.row_data(j).unwrap();
+
+                        if removals.iter().find(|id| **id == task.id).is_some() {
+                            group.get_model().remove(j);
+                            j -= 1;
+                        }
+                    }
+
+                    if group.get_model().row_count() == 0 {
+                        groups_ui.remove(i);
+                        i -= 1;
+                    }
+                }
+            }
         });
 
         self.start_threads();
@@ -90,15 +135,13 @@ impl TaskManager {
         let task = Arc::new(task);
 
         group.get_model().push(TaskData::new(id, name, details));
-        tasks.insert(id, task.clone());
+        tasks.insert(id, task);
         drop(tasks);
 
         if should_start {
-            self.queue = Some(mpsc::channel());
+            self.semaphore.reset();
+            self.semaphore.release();
             self.start_running();
-        }
-        if let Some((s, _)) = &self.queue {
-            s.send(task);
         }
     }
 }
@@ -118,5 +161,42 @@ impl TasksGroup {
 
     pub fn get_model(&self) -> &VecModel<TaskData> {
         self.tasks.as_any().downcast_ref::<VecModel<TaskData>>().unwrap()
+    }
+}
+
+pub struct Semaphore {
+    lock: Mutex<i16>,
+    cvar: Condvar
+}
+
+impl Semaphore {
+    pub fn new(initial: i16) -> Self {
+        Self { lock: Mutex::new(initial), cvar: Condvar::new() }
+    }
+
+    pub fn reset(&self) {
+        let mut lock = self.lock.lock().unwrap();
+        *lock = 0;
+    }
+
+    pub fn acquire(&self) {
+        let mut count = self.lock.lock().unwrap();
+        while *count == 0 {
+            count = self.cvar.wait(count).unwrap();
+        }
+
+        *count -= 1;
+    }
+
+    pub fn release(&self) {
+        let mut count = self.lock.lock().unwrap();
+        *count += 1;
+        self.cvar.notify_one();
+    }
+
+    pub fn release_all(&self) {
+        let mut count = self.lock.lock().unwrap();
+        *count = i16::MAX;
+        self.cvar.notify_all();
     }
 }
