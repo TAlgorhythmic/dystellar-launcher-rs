a714fa9cbb5bb8270d038d0279118f2c4a7893af
Algorhythmics <intivioli@gmail.com>
2025-12-13 13:07:45 +0100

    Refactor

--- src/api/typedef/task_manager.rs
+++ src/api/typedef/task_manager.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, rc::Rc, sync::{Arc, Mutex, atomic::{AtomicI32, AtomicU8, Ordering}}};
+use std::{collections::HashMap, rc::Rc, sync::{Arc, Mutex, atomic::{AtomicBool, AtomicI32, AtomicU8, Ordering}}};
 
 use slint::{Model, ModelRc, Timer, VecModel};
 
@@ -6,43 +6,51 @@ use crate::generated::{TaskData, TaskState, TasksGroup};
 
 static NEXT_TASK_ID: AtomicI32 = AtomicI32::new(0);
 
-pub trait Task: Send + Sync {
+pub trait Task: Send + Sync + 'static {
+    /**
+    * Heavy work, like IO blocking, will be called from random threads
+    */
     fn run(&self);
+
+    /**
+    * Thread safe
+    */
+    fn get_progress(&self) -> f32;
 }
 
-pub struct TaskSharedState {
-    state: AtomicU8,
-    progress: AtomicU8
+pub struct TaskManager<'a> {
+    groups_ui: &'a VecModel<TasksGroup>,
+    tasks: Arc<Mutex<HashMap<i32, Arc<dyn Task>>>>,
+    timer: Timer,
+    running: AtomicBool
 }
 
-pub struct TaskHandle {
-    task: Box<dyn Task>,
-    state: Arc<TaskSharedState>
-}
+impl<'a> TaskManager<'a> {
+    pub fn new(model: &'a VecModel<TasksGroup>) -> Self {
+        Self { groups_ui: model, tasks: Arc::new(Mutex::new(HashMap::new())), timer: Timer::default(), running: AtomicBool::new(false) }
+    }
 
-pub struct TaskManager {
-    groups_ui: Rc<VecModel<TasksGroup>>,
-    tasks: Arc<Mutex<HashMap<i32, Arc<TaskHandle>>>>,
-    timer: Timer
-}
-
-impl TaskManager {
-    pub fn new() -> Self {
-        Self { groups_ui: Rc::new(VecModel::from(vec![])), tasks: Arc::new(Mutex::new(HashMap::new())), timer: Timer::default() }
+    fn start_threads(&self) {
+        self.running.store(true, Ordering::Relaxed);
     }
 
     pub fn submit_task(&mut self, group: &str, name: &str, details: &str, task: impl Task) {
-        let tasks = self.tasks.lock().unwrap();
-        let should_start = tasks.is_empty();
-        drop(tasks);
+        let mut tasks = self.tasks.lock().unwrap();
+        let should_start = tasks.is_empty() && !self.running.load(Ordering::Relaxed);
 
         let group = self.groups_ui.iter().find(|g| g.name.as_str() == group).unwrap_or_else(|| {
             self.groups_ui.push(TasksGroup::new(group));
             self.groups_ui.row_data(self.groups_ui.row_count() - 1).unwrap()
         });
         let id = NEXT_TASK_ID.fetch_add(1, Ordering::Relaxed);
-        
+
         group.get_model().push(TaskData::new(id, name, details));
+        tasks.insert(id, Arc::new(task));
+        drop(tasks);
+
+        if should_start {
+            self.start_threads();
+        }
     }
 }
 
