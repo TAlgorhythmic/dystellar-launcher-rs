da3e02110721f7ad5e3d3245694d41a752f926b6
Algorhythmics <intivioli@gmail.com>
2025-12-13 11:14:48 +0100

    Updates

--- src/api/typedef/task_manager.rs
+++ src/api/typedef/task_manager.rs
@@ -1,38 +1,54 @@
-use std::{rc::Rc, sync::atomic::AtomicU8};
+use std::{collections::HashMap, rc::Rc, sync::{Arc, Mutex, atomic::{AtomicI32, AtomicU8, Ordering}}};
 
 use slint::{Model, ModelRc, Timer, VecModel};
 
-use crate::generated::{TaskData, TasksGroup};
+use crate::generated::{TaskData, TaskState, TasksGroup};
 
+static NEXT_TASK_ID: AtomicI32 = AtomicI32::new(0);
 
+pub trait Task: Send + Sync {
+    fn run(&self);
+}
 
-pub trait Task: Send {
-    fn get_progress(&self) -> f32;
+pub struct TaskSharedState {
+    state: AtomicU8,
+    progress: AtomicU8
 }
 
 pub struct TaskHandle {
     task: Box<dyn Task>,
-    progress: AtomicU8
+    state: Arc<TaskSharedState>
 }
 
 pub struct TaskManager {
     groups_ui: Rc<VecModel<TasksGroup>>,
-    tasks: Vec<TaskHandle>,
+    tasks: Arc<Mutex<HashMap<i32, Arc<TaskHandle>>>>,
     timer: Timer
 }
 
 impl TaskManager {
     pub fn new() -> Self {
-        Self { groups_ui: Rc::new(VecModel::from(vec![])), tasks: vec![], timer: Timer::default() }
+        Self { groups_ui: Rc::new(VecModel::from(vec![])), tasks: Arc::new(Mutex::new(HashMap::new())), timer: Timer::default() }
     }
 
     pub fn submit_task(&mut self, group: &str, name: &str, details: &str, task: impl Task) {
+        let tasks = self.tasks.lock().unwrap();
+        let should_start = tasks.is_empty();
+        drop(tasks);
+
         let group = self.groups_ui.iter().find(|g| g.name.as_str() == group).unwrap_or_else(|| {
             self.groups_ui.push(TasksGroup::new(group));
             self.groups_ui.row_data(self.groups_ui.row_count() - 1).unwrap()
         });
+        let id = NEXT_TASK_ID.fetch_add(1, Ordering::Relaxed);
+        
+        group.get_model().push(TaskData::new(id, name, details));
+    }
+}
 
-        group.get_model().push(TaskData { details: details.into(), name: name.into(), progress: 0.0, state: crate::generated::TaskState::Waiting });
+impl TaskData {
+    pub fn new(id: i32, name: &str, details: &str) -> Self {
+        Self { id, details: details.into(), name: name.into(), progress: 0.0, state: TaskState::Waiting }
     }
 }
 
